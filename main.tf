4f612e26a8a4347ebc52ab078c4190a710224f82
pratik raut <115097202+pratikdevelop@users.noreply.github.com>
2023-12-09 19:15:24 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    render = {
+      source  = "jackall3n/render"
+      version = "1.2.0"
+    }
+  }
+}
+
+provider "render" {
+  api_key = "rnd_emfMYWKtcY8x0Xcs0lXVw0sO1DwS"
+  email   = "pratikraut88895@gmail.com"
+}
+
+resource "render_service" "angular" {
+  name   = "angulr-16"
+  repo   = "https://github.com/pratikdevelop/angulr-16"
+  branch = "master"
+  type   = "static_site"
+
+  static_site_details = {
+    build_command = "npm install --force && npm run build;"
+    publish_path  = "dist"
+    force_https   = true
+
+  }
+}
