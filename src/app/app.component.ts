fa65069fb8ee46087999cb516577d631d0ef32fd
Pratikraut@12345 <pratikraut88895@gmail.com>
2023-12-20 23:23:48 +0530

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component, inject, Input, OnInit, signal } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
+import detectEthereumProvider from '@metamask/detect-provider'
 
 @Component({
   selector: 'app-root',
@@ -9,11 +10,37 @@ import {RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = signal('dds');
+  provider: any;
+  
   
   update(event: any): void {
     this.title.set(event.target.value)
   }
+
+  async ngOnInit(): Promise<void> {
+  this.provider = await detectEthereumProvider()
+
+    if (this.provider) {
+  
+      console.log('Ethereum successfully detected!')
+    
+      // From now on, this should always be true:
+      // provider === window.ethereum
+    
+      // Access the decentralized web!
+    
+      // Legacy providers may only have ethereum.sendAsync
+      const chainId = await this.provider.request({
+        method: 'eth_requestAccounts'
+      })
+    } else {
+    
+      // if the provider is not detected, detectEthereumProvider resolves to null
+      alert('Please install MetaMask!')
+    }
+  }
+
 }
 
